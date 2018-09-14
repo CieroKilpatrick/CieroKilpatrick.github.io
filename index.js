fa5a3cd93c5184570eeecad511023b2fce52d513
Ciero Kilpatrick <40720022+CieroKilpatrick@users.noreply.github.com>
2018-09-14 15:09:54 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,36 +1,39 @@
-// Sticky Header
-$(window).scroll(function() {
+$(document).ready(function() {
 
-    if ($(window).scrollTop() > 100) {
-        $('.main_h').addClass('sticky');
-    } else {
-        $('.main_h').removeClass('sticky');
-    }
-});
+    $(window).scroll(function() {
 
-// Mobile Navigation
-$('.mobile-toggle').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.main_h').removeClass('open-nav');
-    } else {
-        $('.main_h').addClass('open-nav');
-    }
-});
+        if ($(window).scrollTop() > 100) {
+            $('.main_h').addClass('sticky');
+        } else {
+            $('.main_h').removeClass('sticky');
+        }
+    });
+    
+    // Mobile Navigation
+    $('.mobile-toggle').click(function() {
+        if ($('.main_h').hasClass('open-nav')) {
+            $('.main_h').removeClass('open-nav');
+        } else {
+            $('.main_h').addClass('open-nav');
+        }
+    });
+    
+    $('.main_h li a').click(function() {
+        if ($('.main_h').hasClass('open-nav')) {
+            $('.navigation').removeClass('open-nav');
+            $('.main_h').removeClass('open-nav');
+        }
+    });
+    
+    // Navigation Scroll - ljepo radi materem
+    $('nav a').click(function(event) {
+        var id = $(this).attr("href");
+        var offset = 80;
+        var target = $(id).offset().top - offset;
+        $('html, body').animate({
+            scrollTop: target
+        }, 500);
+        event.preventDefault();
+    });
 
-$('.main_h li a').click(function() {
-    if ($('.main_h').hasClass('open-nav')) {
-        $('.navigation').removeClass('open-nav');
-        $('.main_h').removeClass('open-nav');
-    }
-});
-
-// Navigation Scroll - ljepo radi materem
-$('nav a').click(function(event) {
-    var id = $(this).attr("href");
-    var offset = 80;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({
-        scrollTop: target
-    }, 500);
-    event.preventDefault();
 });
