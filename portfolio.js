e033a7b393c1eac3e564e1eb2e5b08fad6c282ac
Ciero Kilpatrick <40720022+CieroKilpatrick@users.noreply.github.com>
2019-12-28 00:06:27 -0500

    Create portfolio.js

---
+++ portfolio.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	$( "#image_one" ).hide();
+
+});
+
+$(document).ready(function() {
+$( "#project_one" ).hover(function() {
+    $( "#image_zero").toggle();
+		$( "#image_one").toggle();
+		$( "p").text("Hi, Mynanme is Ciero");
+	});
+	
+	$( "#project_two" ).hover(function() {
+    $( "#image_zero").toggle();
+		$( "#image_one").toggle();
+		$( "p").text("For academic advisors in certain colleges, state laws require that a school’s students must be designated an abbreviated or comprehensive plan-type that lays out the courses they will take in their first 2-4 terms. On this flow, I helped design how Hobsons’ degree planning software would handle this provision.");
+	});
+	
+	$( "#project_three" ).hover(function() {
+    $( "#image_zero").toggle();
+		$( "#image_one").toggle();
+		$( "p").text("Within the admin side of Hobson’s degree planner tool, a school’s course catalog needed to be accessible for advisors, with helpful and logical navigation. The solution provided here was simple, in a way that made it easy to search and browse.");
+	});
+	
+	$( "#project_four" ).hover(function() {
+    $( "#image_zero").toggle();
+		$( "#image_one").toggle();
+		$( "p").text("The formal way of adding friends to your profile on We Vote is through the 'Connect' page. A user is able to import contacts by signing into their twitter, and entering their friends email addresses, as well as selecting recommended friends, and responding to requests. Here I’ve made designs for how these user flows should happen.");
+	});
+	
+	$( "#project_five" ).hover(function() {
+    $( "#image_zero").toggle();
+		$( "#image_one").toggle();
+		$( "p").text("In the We Vote platform, users and groups show support for different candidates and measures. Given its social nature, we decided to add a pop over whenever a user hovered over another user or group’s thumbnail to learn more about a supporter.");
+	});
+	
+	$( "#project_six" ).hover(function() {
+    $( "#image_zero").toggle();
+		$( "#image_one").toggle();
+		$( "p").text("TechChange provides professional development in technology and social change. For my client, I was given the role of designing a new dashboard in anticipation of current students enrolling in more than one course.");
+	});
+	
+
+});
